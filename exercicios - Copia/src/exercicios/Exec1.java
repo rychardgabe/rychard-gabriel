098320d8eee7bd1fc5e410d2daf337c668092396
Rychard Gabriel <rychard.futurefocus@gmail.com>
2026-02-19 21:22:08 -0300

    Exercicio 1

--- exercicios - Copia/src/exercicios/Exec1.java
+++ exercicios - Copia/src/exercicios/Exec1.java
@@ -1,6 +1,5 @@
 package exercicios;
 
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Exec1 {
@@ -9,24 +8,25 @@ public class Exec1 {
 		
 		Scanner input = new Scanner(System.in);
 		
-		DecimalFormat dec = new DecimalFormat("0.00");
-		
 		System.out.println("Inforem seu cadastro");
 		int cad = input.nextInt();
 		
 		System.out.println("Informe seu nome");
 		String n = input.next();
 		
-		System.out.println("Qual a quantidade de horas semanais foram trabalhadas?");
-		double hs = input.nextDouble();
+		System.out.println("Qual a quantidade de horas trabalhadas?");
+		double h = input.nextDouble();
 		
 		System.out.println("Quanto você ganha por hora?");
 		double vh = input.nextDouble();
 		
-		double sal = (hs * 4)* vh;
+		double sal =  h * vh;
 		
-		System.out.println("Cadastro " + cad + " Colaborador " + n + " Salario: " + " R$ " + dec.format(sal));
-
+		System.out.println("salario: " + sal);
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
 
 	}
 
