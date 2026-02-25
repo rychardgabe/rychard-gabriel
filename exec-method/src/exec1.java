8da28e6f52de3de608ab2f35eef10248524fb26d
Rychard Gabriel <rychard.futurefocus@gmail.com>
2026-02-24 21:28:16 -0300

    exec1 method

---
+++ exec-method/src/exec1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exec1 {
+
+	public static void main(String[] args) {
+		estacoes();
+	}
+	
+	public static void estacoes() {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Informe um valor de 1 à 4");
+		int e = input.nextInt();
+		
+		switch (e) {
+		case 1: {
+			
+		}
+		}
+	}
+
+}
---
+++ exercicios/src/exercicios/Exec5.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exec5 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Informe sua altura");
+
+	}
+
+}
