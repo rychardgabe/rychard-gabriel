9894d7e63e3040a2bec19f015b9d6d34466e8f79
Rychard Gabriel <rychard.futurefocus@gmail.com>
2026-03-10 18:58:57 -0300

    exec1

--- exec-method/src/exec1.java
+++ exec-method/src/exec1.java
@@ -3,20 +3,39 @@ import java.util.Scanner;
 public class exec1 {
 
 	public static void main(String[] args) {
-		estacoes();
+		mostrarEstacoes();
 	}
 	
-	public static void estacoes() {
+	public static void mostrarEstacoes() {
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Informe um valor de 1 à 4");
 		int e = input.nextInt();
 		
-		switch (e) {
-		case 1: {
-			
+		if(e == 1) {
+			verao();
 		}
+		else if(e == 2) {
+			outono();
 		}
+		else if(e == 3) {
+			iverno();
+		}
+		else if(e == 4) {
+			primavera();
+		}
+	}
+	public static void verao() {
+		System.out.println("É verão");
+	}
+	public static void outono() {
+		System.out.println("As folas caem");
+	}
+	public static void iverno() {
+		System.out.println("É inverno");
+	}
+	public static void primavera() {
+		System.out.println("As flores desabrocham");
 	}
 
 }
