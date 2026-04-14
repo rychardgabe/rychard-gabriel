8126478421ab85ea11e1cd745a8c198590a16192
Rychard Gabriel <rychard.futurefocus@gmail.com>
2026-04-14 19:27:21 -0300

    Valores em ordem ao contraria

--- Array-Lis/src/ex02/Ex2.java
+++ Array-Lis/src/ex02/Ex2.java
@@ -1,9 +1,22 @@
 package ex02;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Integer> ListaNumeros = new ArrayList<>();
+		
+		ListaNumeros.add(54);
+		ListaNumeros.add(34);
+		ListaNumeros.add(32);
+		ListaNumeros.add(4);
+		ListaNumeros.add(19);
+		ListaNumeros.sort(Collections.reverseOrder());
+		
+		System.out.println(ListaNumeros);
 
 	}
 
