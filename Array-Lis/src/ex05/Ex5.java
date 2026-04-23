8db48905a3f1ba65704d802041eb967ae111d566
Rychard Gabriel <rychard.futurefocus@gmail.com>
2026-04-23 20:42:16 -0300

    adicionado metodo de verificação de palíndromo

--- Array-Lis/src/ex05/Ex5.java
+++ Array-Lis/src/ex05/Ex5.java
@@ -1,18 +1,31 @@
 package ex05;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Ex5 {
+	public static void main(String[] args) {
 	Scanner p = new Scanner(System.in);
 	
-    System.out.print("Digite uma palavra: ");
-    String palavra = p.nextLine();
+	System.out.print("Digite uma palavra: ");
+	String palavra = p.nextLine();
 	
-	List<Character> palavras = new ArrayList<>();
+	List<Character> list = new ArrayList<Character>();
 	
-	
-	
-	
+	for (char c : palavra.toCharArray()) {
+		list.add(c);
+		}
+    List<Character> listaInvertida = new ArrayList<>(list);
+    Collections.reverse(listaInvertida);
+
+    if (list.equals(listaInvertida)) {
+        System.out.println("É um palíndromo!");
+    } else {
+        System.out.println("Não é um palíndromo.");
+    }
+    p.close();
+
+	}
 }
