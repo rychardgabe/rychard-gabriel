8b3e1d370ded6d2aa53cbd4e2fd52050c8547a68
Rychard Gabriel <rychard.futurefocus@gmail.com>
2026-04-23 19:51:43 -0300

    criando a lista

---
+++ Array-Lis/src/ex05/Ex5.java
@@ -0,0 +1,18 @@
+package ex05;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ex5 {
+	Scanner p = new Scanner(System.in);
+	
+    System.out.print("Digite uma palavra: ");
+    String palavra = p.nextLine();
+	
+	List<Character> palavras = new ArrayList<>();
+	
+	
+	
+	
+}
