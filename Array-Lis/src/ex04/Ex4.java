d4899c40b909e2540499822e54c2765b519c119c
Rychard Gabriel <rychard.futurefocus@gmail.com>
2026-04-23 19:28:11 -0300

    adicionado numero que o usuario adicionou

--- Array-Lis/src/ex04/Ex4.java
+++ Array-Lis/src/ex04/Ex4.java
@@ -2,10 +2,12 @@ package ex04;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Ex4 {
 
 	public static void main(String[] args) {
+		Scanner car = new Scanner(System.in);
 		List<Integer> carros = new ArrayList<>();
 		
 		carros.add(55);
@@ -13,6 +15,11 @@ public class Ex4 {
 		carros.add(345);
 		carros.add(36);
 		carros.add(24);
+		
+		System.out.println("Digite um numero");
+		int numero = car.nextInt();
+		
+		
 		
 	}
 
