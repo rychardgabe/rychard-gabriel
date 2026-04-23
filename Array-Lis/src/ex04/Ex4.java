90b60b4c654989422f018137e963787044b6bee2
Rychard Gabriel <rychard.futurefocus@gmail.com>
2026-04-23 19:33:04 -0300

    adicionado verificação se o numero digitado está no array

--- Array-Lis/src/ex04/Ex4.java
+++ Array-Lis/src/ex04/Ex4.java
@@ -7,20 +7,26 @@ import java.util.Scanner;
 public class Ex4 {
 
 	public static void main(String[] args) {
-		Scanner car = new Scanner(System.in);
-		List<Integer> carros = new ArrayList<>();
+		Scanner n = new Scanner(System.in);
+		List<Integer> numeros = new ArrayList<>();
 		
-		carros.add(55);
-		carros.add(54);
-		carros.add(345);
-		carros.add(36);
-		carros.add(24);
+		numeros.add(55);
+		numeros.add(54);
+		numeros.add(345);
+		numeros.add(36);
+		numeros.add(24);
 		
 		System.out.println("Digite um numero");
-		int numero = car.nextInt();
+		int numero = n.nextInt();
 		
-		
-		
+		if(numeros.contains(numero)) {
+			int indice = numeros.indexOf(numero);
+            System.out.println("Número encontrado!");
+            System.out.println("Índice: " + indice);
+		}else {
+			 System.out.println("Número não encontrado na lista.");
+		}
+		n.close();
 	}
 
 }
