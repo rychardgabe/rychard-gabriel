4a981ad172712fccdc636a3e4f11eac972c5e64e
Rychard Gabriel <rychard.futurefocus@gmail.com>
2026-04-23 19:24:51 -0300

    atividade4

---
+++ Array-Lis/src/ex04/Ex4.java
@@ -0,0 +1,19 @@
+package ex04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+		List<Integer> carros = new ArrayList<>();
+		
+		carros.add(55);
+		carros.add(54);
+		carros.add(345);
+		carros.add(36);
+		carros.add(24);
+		
+	}
+
+}
